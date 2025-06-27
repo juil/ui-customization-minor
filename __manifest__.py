1652bf6d5670e35ecb1101303678b30cad1d6174
Juil <juil@biddingo.com>
2025-06-27 01:14:23 -0700

    Update addon name to 'Minor UI Fixes'
    This better reflects the purpose of this module.

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,13 @@
 {
-    'name': 'UI Customization',
+    'name': 'Minor UI Fixes',
     'version': '17.0.1.0.0',
     'category': 'Technical',
-    'summary': 'Custom UI adjustments for Odoo',
+    'summary': 'Minor UI fixes for Odoo v17.',
     'depends': ['web', 'hr_holidays'], # Ensure this depends on the modules whose styles you're affecting
     'data': [], # No XML data needed for this specific CSS override
     'assets': {
         'web.assets_backend': [ # This tells Odoo to load your SCSS file in the backend
-            'ui-customization/static/src/scss/custom_styles.scss',
+            'ui-customization-minor/static/src/scss/custom_styles.scss',
         ],
     },
     'installable': True,
